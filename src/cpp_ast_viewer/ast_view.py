ff17714d2393e641d7f8522f1e9f0560e8f87557
SyaoranY <yanghangxxx@gmail.com>
2026-09-05 21:54:35 -0700

    bugfix: use Cursor as key to avoid hash collision when using Cursor.hash as key

--- src/cpp_ast_viewer/ast_view.py
+++ src/cpp_ast_viewer/ast_view.py
@@ -36,7 +36,7 @@ class AstTreeView(QTreeView):
         self.clicked.connect(self._on_clicked)
         self.expanded.connect(self._on_expanded)
 
-        self._parent_dict_cache: dict[TranslationUnit, dict[int, Cursor]] = {}
+        self._parent_dict_cache: dict[TranslationUnit, dict[Cursor, Cursor]] = {}
 
     def reset_all(self):
         self._parent_dict_cache = {}
@@ -50,19 +50,19 @@ class AstTreeView(QTreeView):
         if parent_dict is None:
             self._parent_dict_cache[tu] = self._build_parent_dict(tu)
 
-    def _build_parent_dict(self, tu) -> dict[int, Cursor]:
-        parent_dict: dict[int, Cursor] = {}
+    def _build_parent_dict(self, tu) -> dict[Cursor, Cursor]:
+        parent_dict: dict[Cursor, Cursor] = {}
         root = tu.cursor  # root cursor not included in parent_dict
-        visited: set[int] = {root.hash}
+        visited: set[Cursor] = {root}
         stack = [root]
 
         while stack:
             parent = stack.pop()
             for child in parent.get_children():
-                if child.hash in visited:
+                if child in visited:
                     continue
-                visited.add(child.hash)
-                parent_dict[child.hash] = parent
+                visited.add(child)
+                parent_dict[child] = parent
                 stack.append(child)
 
         return parent_dict
@@ -130,9 +130,9 @@ class AstTreeView(QTreeView):
         cursor_path = []
         current = cursor
         tu = self._model.invisibleRootItem().data()
-        while current.hash in self._parent_dict_cache[tu]:
+        while current in self._parent_dict_cache[tu]:
             cursor_path.append(current)
-            current = self._parent_dict_cache[tu][current.hash]
+            current = self._parent_dict_cache[tu][current]
         cursor_path.reverse()
         return cursor_path
 
